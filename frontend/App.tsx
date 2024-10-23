364794b92efb485d30077d7ce4abd6d6d1c8d66d
sambhujimmi <sambhujimmi@gmail.com>
2024-10-24 03:48:51 +0530

    Added colors and tested

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Colors } from "./constants/Colors";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Sudoku Solver</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Sudoku Solver</Text>
+      <Pressable style={styles.button}>
+        <Text style={{ color: Colors.light.text }}>Scan</Text>
+      </Pressable>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: Colors.dark.background,
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    color: Colors.dark.text,
+  },
+  button: {
+    backgroundColor: Colors.dark.primary,
   },
 });
