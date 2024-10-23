4d3b20896b22da937af229462e3c89a7eadd228e
sambhujimmi <sambhujimmi@gmail.com>
2024-10-24 03:46:26 +0530

    Added colors

---
+++ frontend/constants/Colors.ts
@@ -0,0 +1,14 @@
+import { BackHandler } from "react-native";
+
+export const Colors = {
+  light: {
+    background: "#fff",
+    text: "#000",
+    primary: "red",
+  },
+  dark: {
+    background: "#000",
+    text: "#fff",
+    primary: "yellow",
+  },
+};
